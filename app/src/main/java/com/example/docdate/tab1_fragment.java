d248199f0bb3a178d08eabbcb3ea5ac5a01beff1
SimonMFuchs <simon.fuchs10@gmail.com>
2022-01-02 14:00:01 +0100

    start screen view

--- app/src/main/java/com/example/docdate/tab1_fragment.java
+++ app/src/main/java/com/example/docdate/tab1_fragment.java
@@ -3,10 +3,13 @@ package com.example.docdate;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +61,8 @@ public class tab1_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tab1_layout, container, false);
     }
+
+
 }
